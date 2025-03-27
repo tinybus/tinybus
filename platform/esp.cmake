1b5b8b608331f275dda6aaa02b5680d69f7cc439
Marcel Graber <marcel@clever.design>
2025-03-27 13:34:10 +0100

    fix: Redesign build system

--- platform/esp.cmake
+++ platform/esp.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.20)
 idf_component_register()
 
 tiny_library_named(tinybus)
-tiny_include_directories(${PROJECT_DIR}/include)
+tiny_library_include_directories_public(${PROJECT_DIR}/include)
 tiny_library_link_libraries(tiny)
 add_subdirectory(${PROJECT_DIR}/src)
 
