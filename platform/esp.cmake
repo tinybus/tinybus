f8ea8cf301d7d9c6c68aa0e4b964babd38753007
Marcel Graber <marcel@clever.design>
2025-03-28 07:25:42 +0100

    fix: rename tiny to ty

--- platform/esp.cmake
+++ platform/esp.cmake
@@ -2,9 +2,9 @@ cmake_minimum_required(VERSION 3.20)
 
 idf_component_register()
 
-tiny_library_named(tinybus)
-tiny_library_include_directories_public(${PROJECT_DIR}/include)
-tiny_library_link_libraries(tiny)
+ty_library_named(tinybus)
+ty_library_include_directories_public(${PROJECT_DIR}/include)
+ty_library_link_libraries(tiny)
 add_subdirectory(${PROJECT_DIR}/src)
 
 target_link_libraries(${COMPONENT_LIB} INTERFACE tinybus)
